60fab5a1f5f6b1a28cad904e2e0e5ecfe34c7c89
nizp <nzp890524@163.com>
2016-05-18 15:31:44 +0800

    chang10

--- 1.js
+++ 1.js
@@ -16,3 +16,7 @@ Drag.prototype.fnDown = function(ev){
 		_this.fnMove(e);
 	}
 }
+Drag.prototype.fnMove = function(ev){
+	this.obj.style.left = ev.pageX - this.disX + 'px';
+	this.obj.style.top = ev.pageY - this.disY + 'px';
+}
