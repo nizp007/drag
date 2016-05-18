07ee599db606bc3fdb4ff06c733eaf9e31232920
nizp007 <419757318@qq.com>
2016-05-18 17:02:41 +0800

    fnDown

--- 1.js
+++ 1.js
@@ -7,3 +7,17 @@ function Drag(id){
 	},false);
 }
 
+Drag.prototype.fnDown = function(ev){
+	this.disX = ev.pageX - this.obj.offsetLeft;
+	this.disY = ev.pageY - this.obj.offsetTop;
+	var _this = this;
+	document.addEventListener('mousemove',move,false);
+	document.addEventListener('mouseup',up,false);
+	function move(ev){
+		var e = ev || window.event;
+		_this.fnMove(e);
+	}
+	function up(){
+		_this.fnUp(move,up);
+	}
+}
