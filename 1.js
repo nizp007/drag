581aeb8e92316097354359fd6e4e787404f4b8cc
nizp <nzp890524@163.com>
2016-05-18 15:12:21 +0800

    chang8

---
+++ 1.js
@@ -0,0 +1,18 @@
+function Drag(id){
+	this.obj = document.getElementById(id);
+	var _this = this;
+	this.obj.addEventListener('mousedown',function(ev){
+		var e = ev || window.event;
+		_this.fnDown(e);
+	},false);
+}
+Drag.prototype.fnDown = function(ev){
+	this.disX = ev.clientX - this.obj.offsetLeft;
+	this.disY = ev.clientY - this.obj.offsetTop;
+	var _this = this;
+	document.addEventListener('mousemove',move,false);
+	function move(ev){
+		var e = ev || window.event;
+		_this.fnMove(e);
+	}
+}
